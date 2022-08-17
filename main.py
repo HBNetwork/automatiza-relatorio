ecdf38609d1a46e5376509c61e96b9333c8369f2
Henrique Bastos <henrique@bastos.net>
2022-08-16 21:32:27 -0300

    Refatora o script em um programa.

--- main.py
+++ main.py
@@ -3,48 +3,56 @@ from datetime import datetime
 from pdf_reports import pug_to_html, write_report
 
 import sys
-parametro = []
-for param in sys.argv :
-    parametro.append(param)
-
-#print(parametro.pop())
-
-#pip install pdf_reports -q
 
 
-planilha = pd.read_excel(parametro.pop())
-#planilha = pd.read_excel("planilhas/Vendas_diogo.xlsx")
+def cli():
+    if len(sys.argv) != 2:
+        sys.exit(1)
 
-# Calculos da planilha
-
-faturamento = planilha[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
-faturamento = faturamento.sort_values(by='Valor Final', ascending=False)
+    return sys.argv[-1]
 
 
-quantidade = planilha[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
-quantidade = quantidade.sort_values(by='ID Loja', ascending=False)
+def le_planilha(filename):
+    return pd.read_excel(filename)
 
 
-hoje = datetime.now().date()
+def calcula_faturamento(planilha):
+    faturamento = planilha[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+    return faturamento.sort_values(by='Valor Final', ascending=False)
 
 
-html = pug_to_html('planilhas/modelo_vendas.pug',planilha=planilha,hoje=hoje, faturamento=faturamento)
+def calcula_quantidade(planilha):
+    quantidade = planilha[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+    return quantidade.sort_values(by='ID Loja', ascending=False)
 
 
-write_report(html, 'vendas.pdf')
-
-faturamento.iloc[0:5].reset_index()
-
-quantidade.iloc[:25].reset_index()
-
-ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
-ticket_medio = ticket_medio.rename(columns={0: 'Ticket Medio'})
+def calcula_ticket_medio(faturamento, quantidade):
+    ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
+    return ticket_medio.rename(columns={0: 'Ticket Medio'})
 
 
-faturamento = faturamento
+def calcula_resumo(faturamento, quantidade, ticket_medio):
+    faturamento.iloc[0:5].reset_index()
+    quantidade.iloc[:25].reset_index()
+
+    tabela_resumo = faturamento.join(quantidade).join(ticket_medio)
+    return tabela_resumo.rename(columns={'Valor Final': 'Faturamento Total'})
 
 
-tabela_resumo = faturamento.join(quantidade).join(ticket_medio)
-tabela_resumo = tabela_resumo.rename(columns={'Valor Final': 'Faturamento Total'})
+def hoje(now=datetime.now):
+    return now().date()
 
 
+def escreve_pdf(filename, pugfile, data=hoje, **params):
+    html = pug_to_html(pugfile, hoje=data(), **params)
+    write_report(html, filename)
+
+
+if __name__ == "__main__":
+    filename = cli()
+    planilha = le_planilha(filename)
+    faturamento = calcula_faturamento(planilha)
+    quantidade = calcula_quantidade(planilha)
+    ticket_medio = calcula_ticket_medio(faturamento, quantidade)
+    resumo = calcula_resumo(faturamento, quantidade, ticket_medio)
+    escreve_pdf("vendas.pdf", pugfile="planilhas/modelo_vendas.pug", planilha=planilha, faturamento=faturamento)
